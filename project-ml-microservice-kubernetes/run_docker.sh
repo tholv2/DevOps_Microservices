9330d18610c713d4856db03c0c333278e718bc64
Ubuntu <ubuntu@ip-172-31-67-130.ec2.internal>
2023-08-03 07:37:17 +0000

    commit

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build --tag tholv2-udacity:v1.0.0 .
 
 # Step 2: 
 # List docker images
+docker image list
 
 # Step 3: 
 # Run flask app
+docker run -p 8000:80 tholv2-udacity:v1.0.0
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="tholv2/app"
+imagename="tholv2-udacity:v1.0.0"
 
-# Step 2:  
+# Step 2:
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag $imagename $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
