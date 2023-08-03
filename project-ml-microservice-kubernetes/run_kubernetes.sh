193b24138d821db9f98f0aab4dd7af1fdd3d209c
Ubuntu <ubuntu@ip-172-31-67-130.ec2.internal>
2023-08-03 07:39:32 +0000

    commit

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="tholv2/app"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment udacity-app --image $dockerpath
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+podname=$(kubectl get pods --no-headers -o custom-columns=":metadata.name")
+kubectl port-forward $podname 8000:80
